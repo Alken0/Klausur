58e5b00591bb63f31d770b6e4b8f569d1f364aac
Jonas <burgeljonas@gmail.com>
2020-10-04 21:55:02 +0200

    added new lib class for lists
    supported functions:
    - sort
    - reverse
    - shuffle
    - filter

---
+++ src/lib/ListUtil.java
@@ -0,0 +1,44 @@
+package lib;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class ListUtil {
+
+	public static <T> List<T> shuffle(List<T> list) {
+		var shuffledList = new ArrayList<>(list);
+		Collections.shuffle(shuffledList);
+		return shuffledList;
+	}
+
+	public static <T> List<T> reverse(List<T> list) {
+		var reversedList = new ArrayList<>(list);
+		Collections.reverse(reversedList);
+		return reversedList;
+	}
+
+	// compareFunction example: String::length
+	public static <T, U extends Comparable<? super U>> List<T> sort(List<T> list, Function<T, U> compareFunction) {
+		return list.stream()
+				.sorted(Comparator.comparing(compareFunction))
+				.collect(Collectors.toList());
+	}
+
+	public static <T> List<T> sort(List<T> list) {
+		return list.stream()
+				.sorted()
+				.collect(Collectors.toList());
+	}
+
+	// predicate example: i -> i < 3
+	public static <T> List<T> filter(List<T> list, Predicate<T> filterFunction) {
+		return list.stream()
+				.filter(filterFunction)
+				.collect(Collectors.toList());
+	}
+}
---
+++ src/test_lib/ListUtilTest.java
@@ -0,0 +1,57 @@
+package test_lib;
+
+import lib.ListUtil;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+
+public class ListUtilTest {
+
+	@Test
+	public void shuffle() {
+		// could fail due to randomness
+		var input = Arrays.asList(1, 2);
+		var firstElements = new ArrayList<>();
+		for (int i = 0; i < 10; i++) {
+			var firstElement = ListUtil.shuffle(input).get(0);
+			assertEquals(Arrays.asList(1, 2), input);
+			firstElements.add(firstElement);
+		}
+		assertTrue(firstElements.contains(1));
+		assertTrue(firstElements.contains(2));
+	}
+
+	@Test
+	public void reverse() {
+		var input = Arrays.asList(5, 3, 1, 2, 4);
+		var result = ListUtil.reverse(input);
+		assertEquals(Arrays.asList(5, 3, 1, 2, 4), input);
+		assertEquals(Arrays.asList(4, 2, 1, 3, 5), result);
+	}
+
+	@Test
+	public void sortNoParameter() {
+		var input = Arrays.asList(5, 3, 1, 2, 4);
+		var result = ListUtil.sort(input);
+		assertEquals(Arrays.asList(1, 2, 3, 4, 5), result);
+	}
+
+	@Test
+	public void sortWithCompareFunction() {
+		var input = Arrays.asList("c", "ab", "lklaf", "sfe");
+		var result = ListUtil.sort(input, String::length);
+		assertEquals(Arrays.asList("c", "ab", "sfe", "lklaf"), result);
+	}
+
+	@Test
+	public void filter() {
+		var input = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+		var result = ListUtil.filter(input, i -> i < 5);
+		assertEquals(Arrays.asList(1, 2, 3, 4), result);
+	}
+}
