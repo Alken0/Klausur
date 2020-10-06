20f74526f51abf339142c26f3fec741e6450e7f8
Jonas <burgeljonas@gmail.com>
2020-10-06 13:08:45 +0200

    added contributors

--- src/lib/Contributors.java
+++ src/lib/Contributors.java
@@ -3,7 +3,10 @@ package lib;
 /**
  * This library is a project of the following students:
  * <p>
- * 7548389
+ * 7548389, <br>
+ * 5169807, <br>
+ * 6991560
+ * </p>
  */
 
 public class Contributors {
