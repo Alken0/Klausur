70b4dd2fd51a1ee0e2a32f7bdbe30d7fba4bbc82
Jonas <burgeljonas@gmail.com>
2020-10-08 16:35:55 +0200

    delete tests

--- src/module-info.java
+++ src/module-info.java
@@ -1,10 +1,3 @@
 module exam {
 	requires java.desktop;
-	// you still need to download javadoc yourself
-	//intellij: file/project structure/SDKs/<ur_sdk>/documentation_paths
-
-	// delete the following lines + test_lib if testing is not desired
-	// otherwise you need to add junit to your classpath (hover on imports in a testfile)
-	requires junit;
-	exports test_lib;
 }
